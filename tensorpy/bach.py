4eff0a2b954147f1d1d3dbb3e785e07458176a93
Gerardo Castaneda <gcastaneda@yattas.com>
2017-05-09 10:15:44 -0500

    change home page action

--- tensorpy/bach.py
+++ tensorpy/bach.py
@@ -1,15 +1,7 @@
-#!/home/ubuntu/anaconda2/envs/tf1.1_py2.7/bin/python
-import cgitb
-import cgi
-
 import os
 import random
 import rnn_rbm_generate
 
-cgitb.enable()
-
-# Append paths to python files so we can import them
-# sys.path.append('../')
 
 # call to generate a new song
 rnn_rbm_generate.main("parameter_checkpoints/pretrained.ckpt")
@@ -42,47 +34,4 @@ os.system(cmd)
 # os.system(cmd2)
 
 
-print("Content-type: text/html")
-print("")
-print("<!DOCTYPE html>")
-print("<html>")
-print("<head>")
-print("<title>TensorWeb Music</title>")
-
-print("<meta charset=\"utf-8\"/>")
-
-print(
-"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/css/swipebox.min.css\">")
-print("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css\"/>")
-print("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css\"/>")
-print("<link rel=\"stylesheet\" href=\"../tensorweb/css/main.css\">")
-print("<link rel=\"stylesheet\" href=\"../tensorweb/css/gallery.css\">")
-
-print("<script src=\"https://code.jquery.com/jquery-1.11.3.min.js\"></script>")
-print(
-"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/js/jquery.swipebox.min.js\"></script>")
-print("<script>")
-print("$('.swipebox').swipebox();")
-print("</script>")
-
-print("</head>")
-
-print("<body>")
-
-print("<div class=\"header\"> ")
-print("<h1>TensorWeb Music</h1>")
-print("</div>")
-
-print("<div class=\"formsubmit\">")
-print("<h2>Your Song Is ready!</h2>")
-print("</div>")
-
-print("<div class=\"footer\">")
-print(
-"<p>Tensor Music | <a href=\"../tensorweb/index.html\">Create</a> and <a href=\"../tensorweb/play.html\">Play</a></p>")
-print("</div>")
-
-print("</body>")
-
-print("</html>")
 
