311a2c014d9cb14663482a6d51a9a17fcd7a4fb0
Gerardo Castaneda <gcastaneda@yattas.com>
2017-05-09 08:55:23 -0500

    test cleaner html

--- tensorpy/playfile.py
+++ tensorpy/playfile.py
@@ -5,58 +5,61 @@ import os
 
 cgitb.enable()
 
-print("Content-type: text/html")
-print("")
-print("<!DOCTYPE html>")
-print("<html>")
-print("<head>")
-print("<title>TensorWeb Music</title>")
-
-print("<meta charset=\"utf-8\"/>")
-
 print(
-"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/css/swipebox.min.css\">")
-print("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css\"/>")
-print("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css\"/>")
-print("<link rel=\"stylesheet\" href=\"../tensorweb/css/main.css\">")
-print("<link rel=\"stylesheet\" href=\"../tensorweb/css/gallery.css\">")
+"Content-type: text/html"
+""
+"<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<title>TensorWeb Music</title>"
 
-print("<script src=\"https://code.jquery.com/jquery-1.11.3.min.js\"></script>")
-print(
-"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/js/jquery.swipebox.min.js\"></script>")
-print("<script>")
-print("$('.swipebox').swipebox();")
-print("</script>")
+"<meta charset=\"utf-8\"/>"
 
-print("</head>")
 
-print("<body>")
+"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/css/swipebox.min.css\">"
+"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css\"/>"
+"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css\"/>"
+"<link rel=\"stylesheet\" href=\"../tensorweb/css/main.css\">"
+"<link rel=\"stylesheet\" href=\"../tensorweb/css/gallery.css\">"
 
-print("<div class=\"header\"> ")
-print("<h1>TensorWeb Music</h1>")
-print("</div>")
+"<script src=\"https://code.jquery.com/jquery-1.11.3.min.js\"></script>"
+"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/js/jquery.swipebox.min.js\"></script>"
+"<script>"
+"$('.swipebox').swipebox();"
+"</script>"
 
-print("<div class=\"formsubmit\">")
-print("<iframe name=\"mp3frame\" src=\"\"></iframe>")
-print("</div>")
+"</head>"
 
-print("<div class=\"formsubmit\">")
-print("<nav><ul>")
+"<body>"
 
-print ("<li><b>Please select the file you wish to play</b></li>")
+"<div class=\"header\"> "
+"<h1>TensorWeb Music</h1>"
+"</div>"
+
+"<div class=\"formsubmit\">"
+"<iframe name=\"mp3frame\" src=\"\"></iframe>"
+"</div>"
+
+"<div class=\"formsubmit\">"
+"<nav><ul>"
+
+"<li><b>Please select the file you wish to play</b></li>"
+)
 
 for file in os.listdir("../tensorweb/mp3files"):
     #if file.endswith(".mp3"):
     print ("<li><a href=\"/tensorweb/mp3files/%s\" target=\"mp3frame\">%s</a></li>" % (file, file))
 
-print("</ul></nav>")
-print("</div>")
-
-print("<div class=\"footer\">")
 print(
-"<p>Tensor Music | <a href=\"../tensorweb/index.html\">Create</a> and <a href=\"../tensorweb/play.html\">Play</a></p>")
-print("</div>")
+"</ul></nav>"
+"</div>"
 
-print("</body>")
+"<div class=\"footer\">"
 
-print("</html>")
+"<p>Tensor Music | <a href=\"../tensorweb/index.html\">Create</a> and <a href=\"../tensorweb/play.html\">Play</a></p>"
+"</div>"
+
+"</body>"
+
+"</html>"
+)
---
+++ tensorpy/playfile_backup.py
@@ -0,0 +1,62 @@
+#!/home/ubuntu/anaconda2/envs/tf1.1_py2.7/bin/python
+import cgitb
+import cgi
+import os
+
+cgitb.enable()
+
+print("Content-type: text/html")
+print("")
+print("<!DOCTYPE html>")
+print("<html>")
+print("<head>")
+print("<title>TensorWeb Music</title>")
+
+print("<meta charset=\"utf-8\"/>")
+
+print(
+"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/css/swipebox.min.css\">")
+print("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css\"/>")
+print("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css\"/>")
+print("<link rel=\"stylesheet\" href=\"../tensorweb/css/main.css\">")
+print("<link rel=\"stylesheet\" href=\"../tensorweb/css/gallery.css\">")
+
+print("<script src=\"https://code.jquery.com/jquery-1.11.3.min.js\"></script>")
+print(
+"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery.swipebox/1.4.1/js/jquery.swipebox.min.js\"></script>")
+print("<script>")
+print("$('.swipebox').swipebox();")
+print("</script>")
+
+print("</head>")
+
+print("<body>")
+
+print("<div class=\"header\"> ")
+print("<h1>TensorWeb Music</h1>")
+print("</div>")
+
+print("<div class=\"formsubmit\">")
+print("<iframe name=\"mp3frame\" src=\"\"></iframe>")
+print("</div>")
+
+print("<div class=\"formsubmit\">")
+print("<nav><ul>")
+
+print ("<li><b>Please select the file you wish to play</b></li>")
+
+for file in os.listdir("../tensorweb/mp3files"):
+    #if file.endswith(".mp3"):
+    print ("<li><a href=\"/tensorweb/mp3files/%s\" target=\"mp3frame\">%s</a></li>" % (file, file))
+
+print("</ul></nav>")
+print("</div>")
+
+print("<div class=\"footer\">")
+print(
+"<p>Tensor Music | <a href=\"../tensorweb/index.html\">Create</a> and <a href=\"../tensorweb/play.html\">Play</a></p>")
+print("</div>")
+
+print("</body>")
+
+print("</html>")
